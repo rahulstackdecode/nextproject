d0cb9583047ad01441c30d31ea4b0e535d2df179
rahulstackdecode <rahulstackdecode@gmail.com>
2025-04-10 12:41:44 +0530

    text update

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -29,7 +29,7 @@ const Nav = ({ openNav }: Props) => {
         navBg ? 'bg-white shadow-md' : ''
       }`}
     >
-      <div className="max-w-7xl mx-auto flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
+      <div className="max-w-7xl mx-auto flex items-center h-full justify-between w-[90%] xl:w-[80%]">
         {/* Logo */}
         <h1 className="text-xl md:text-3xl font-bold">
           <span className="text-3xl md:text-4xl text-black">Test</span>
